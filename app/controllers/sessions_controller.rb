e5e1e02f560539bd0cffc34fb766d24c45c6b94b
Heidi Hlavinka <hchlav@gmail.com>
2023-02-17 16:54:25 -0600

    Created login view. Testing error messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,25 @@
 class SessionsController < ApplicationController
 
   get '/login' do
-
+    erb :'/sessions/login'
   end
-
-
 
   post '/login' do
-
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:id] = @user.id
+      redirect to '/'
+    else
+      flash[:error] = "Incorrect email or password."
+      redirect '/sessions/login'
+    end
   end
-
 
 
   get '/logout' do
     session.clear
     redirect '/'
   end
-
-
-
 
 
 end
